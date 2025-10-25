062c9d9ce006bb51fd4fe1ba639849c77a364a64
Miguel Leal Fernández <miguileal8@gmail.com>
2025-10-25 03:11:01 +0200

    login update 2

--- api/auth/login.js
+++ api/auth/login.js
@@ -2,6 +2,8 @@
 
 export default async function handler(req, res) {
   const { WHOP_CLIENT_ID, WHOP_REDIRECT_URI } = process.env;
+  console.log("Client ID que se usa:", WHOP_CLIENT_ID)
+  console.log("Redirect URI que se usa:", WHOP_REDIRECT_URI)
 
   if (!WHOP_CLIENT_ID || !WHOP_REDIRECT_URI) {
     return res.status(500).json({ error: "Missing environment variables" });
